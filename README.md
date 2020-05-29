0076f97b4b1555bc38c6ccc5035926400fcd15c5
matt <admin@itcloud.ovh>
2020-05-29 03:08:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nextcloud-libvirt-terraform-ansible
---
+++ nextcloud_ansible/roles/php/files/phpinfo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
+
+</body>
+</html>
